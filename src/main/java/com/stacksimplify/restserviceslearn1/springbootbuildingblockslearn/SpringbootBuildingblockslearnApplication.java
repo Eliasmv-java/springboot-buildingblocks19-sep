3ca400c83564acbd2e878b4206d054abee34f0c0
Elias <elias.varkey@gmail.com>
2020-09-19 11:35:03 +0100

    First Commit

---
+++ src/main/java/com/stacksimplify/restserviceslearn1/springbootbuildingblockslearn/SpringbootBuildingblockslearnApplication.java
@@ -0,0 +1,13 @@
+package com.stacksimplify.restserviceslearn1.springbootbuildingblockslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBuildingblockslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBuildingblockslearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stacksimplify/restserviceslearn1/springbootbuildingblockslearn/SpringbootBuildingblockslearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stacksimplify.restserviceslearn1.springbootbuildingblockslearn;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class SpringbootBuildingblockslearnApplicationTests {
+ 
+	@Test
+	void contextLoads() {
+	}
+
+}
