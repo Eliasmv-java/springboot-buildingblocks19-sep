d418761212b20cfacedc4b910d1a2fb7d4961a63
Elias <elias.varkey@gmail.com>
2020-09-19 12:05:44 +0100

    Second Commit

--- src/main/java/com/stacksimplify/restserviceslearn1/springbootbuildingblockslearn/SpringbootBuildingblockslearnApplication.java
+++ src/main/java/com/stacksimplify/restserviceslearn1/springbootbuildingblockslearn/SpringbootBuildingblockslearnApplication.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restserviceslearn1.springbootbuildingblockslearn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//
 @SpringBootApplication
 public class SpringbootBuildingblockslearnApplication {
 
